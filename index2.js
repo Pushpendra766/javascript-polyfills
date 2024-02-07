cb7e05692222871ab4c02d6a26f22f6a648f882e
Pushpendra766 <pushpendrachandravanshi>
2024-02-07 10:16:08 +0530

    map, filter, reduce, forEach done

--- index2.js
+++ index2.js
@@ -1,3 +1,5 @@
+// map
+
 Array.prototype.polyMap = function (callback, argThis) {
   var newArr = [];
   for (let i = 0; i < this.length; i++) {
@@ -6,7 +8,41 @@ Array.prototype.polyMap = function (callback, argThis) {
   return newArr;
 };
 
+// filter
+
+Array.prototype.polyFilter = function (callback, argThis) {
+  var newArr = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callback.call(argThis, this[i], i, this)) {
+      newArr.push(this[i]);
+    }
+  }
+  return newArr;
+};
+
+// reduce
+
+Array.prototype.polyReduce = function (callback, initialValue) {
+  var result = initialValue;
+  for (let i = 0; i < this.length; i++) {
+    if (result !== undefined) {
+      result = callback(result, this[i], i, this);
+    } else {
+      result = this[i];
+    }
+  }
+  return result;
+};
+
+// forEach
+
+Array.prototype.polyForEach = function (callback, argThis) {
+  for (let i = 0; i < this.length; i++) {
+    callback.call(argThis, this[i], i, this);
+  }
+};
+
 const arr = [34, 1, 6, 3, 12, 53, 6];
 
-console.log(arr.map((el) => console.log(el)));
-console.log(arr.polyMap((el) => console.log(el * 10)));
+console.log(arr.forEach((el) => console.log(el * 10)));
+console.log(arr.polyForEach((el) => console.log(el * 10)));
